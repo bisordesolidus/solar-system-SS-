51d0c9e034cd8ca5ca5e0ad346a82a34bbe8ea7d
arimgenov <152085007+arimgenov@users.noreply.github.com>
2023-11-29 14:39:35 +0300

    Update solar_input.py

--- solar_input.py
+++ solar_input.py
@@ -19,10 +19,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -69,8 +73,16 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    
+    type, R, color, m, x, y, Vx, Vy = line.split(' ')
 
+    planet.color = color
+    planet.R = float(R)
+    planet.m = float(m)
+    planet.y = float(y)
+    planet.x = float(x)
+    planet.Vy = float(Vy)
+    planet.Vx = float(Vx)
 
 def write_space_objects_data_to_file(output_filename, space_objects):
     """Сохраняет данные о космических объектах в файл.
